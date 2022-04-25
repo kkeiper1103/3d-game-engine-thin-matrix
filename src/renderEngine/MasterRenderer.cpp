188f3e089858eeacb2b16a7eeb2fee5670c5da51
Kyle Keiper <kkeiper1103@gmail.com>
2022-04-25 16:53:30 -0400

    added error check for empty entityList

--- src/renderEngine/MasterRenderer.cpp
+++ src/renderEngine/MasterRenderer.cpp
@@ -28,7 +28,13 @@ void MasterRenderer::render(const Light &sun, const Camera &camera) {
 }
 
 void MasterRenderer::processEntity(EntityPtr &entity) {
-    TexturedModel entityModel = entity->getModel();
+
+    // this allows us to check for nullptr instead of crashing on null memory
+    auto entityModel = &entity->getModel();
+
+    // if the entityModel is null (doesn't exist), just return. In effect, this allows the program to *not crash*
+    // when an empty list of entities is passed to it
+    if(entityModel == nullptr) return;
 
     // much simpler than java; we don't have to worry about creating the new array list, or if it's found or anything,
     // as it just exists due to the declaration
@@ -36,5 +42,5 @@ void MasterRenderer::processEntity(EntityPtr &entity) {
     // also, we do *not* move the entity here, as that would cause it to be deleted when the renderer clears its
     // list of entities at the end of each frame. This creates a copy of the model which is then destroyed at the end
     // of the rendering pass
-    entities[entityModel].push_back( entity );
+    entities[*entityModel].push_back( entity );
 }
